b34b4cb1a4a76898f3c8cd575e0ca6b16cc6da41
Grant McAllister <dmcallister@eab.com>
2025-02-13 21:30:15 -0600

    Small change

--- nvim/.config/nvim/lua/grantmca/plugins/nvim-devdocs.lua
+++ nvim/.config/nvim/lua/grantmca/plugins/nvim-devdocs.lua
@@ -1,9 +1,15 @@
-return {
-  "luckasRanarison/nvim-devdocs",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  opts = {}
-}
+local env_var = os.getenv("DOCS")
+
+if env_var == "on" then
+  return {
+    "luckasRanarison/nvim-devdocs",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {}
+  }
+else
+  return {}
+end
