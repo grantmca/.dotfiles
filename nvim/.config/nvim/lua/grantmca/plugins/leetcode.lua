af8f2537532af7fbd0110b0305b934cb18b28b00
Grant McAllister <dmcallister@eab.com>
2025-02-13 21:31:53 -0600

    Adding leet code

---
+++ nvim/.config/nvim/lua/grantmca/plugins/leetcode.lua
@@ -0,0 +1,103 @@
+local env_var = os.getenv("LEET")
+if env_var == "on" then
+  return {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      -- "ibhagwan/fzf-lua",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      ---@type string
+      arg = "leetcode.nvim",
+
+      lang = "python",
+
+      cn = { -- leetcode.cn
+        enabled = false, ---@type boolean
+        translator = true, ---@type boolean
+        translate_problems = true, ---@type boolean
+      },
+
+      storage = {
+        home = vim.fn.stdpath("data") .. "/leetcode",
+        cache = vim.fn.stdpath("cache") .. "/leetcode",
+      },
+
+      ---@type table<string, boolean>
+      plugins = {
+        non_standalone = false,
+      },
+
+      ---@type boolean
+      logging = true,
+
+      injector = {},
+
+      cache = {
+        update_interval = 60 * 60 * 24 * 7, ---@type integer 7 days
+      },
+
+      console = {
+        open_on_runcode = true, ---@type boolean
+
+        dir = "row",
+
+        size = {
+          width = "90%",
+          height = "75%",
+        },
+
+        result = {
+          size = "60%",
+        },
+
+        testcase = {
+          virt_text = true, ---@type boolean
+
+          size = "40%",
+        },
+      },
+
+      description = {
+        position = "left",
+
+        width = "40%",
+
+        show_stats = true,
+      },
+
+      picker = { provider = nil },
+
+      hooks = {
+        ---@type fun()[]
+        ["enter"] = {},
+
+        ---@type fun(question: lc.ui.Question)[]
+        ["question_enter"] = {},
+
+        ---@type fun()[]
+        ["leave"] = {},
+      },
+
+      keys = {
+        toggle = { "q" }, ---@type string|string[]
+        confirm = { "<CR>" }, ---@type string|string[]
+
+        reset_testcases = "r", ---@type string
+        use_testcase = "U", ---@type string
+        focus_testcases = "H", ---@type string
+        focus_result = "L", ---@type string
+      },
+
+      theme = {},
+
+      ---@type boolean
+      image_support = false,
+    },
+  }
+else
+  return {}
+end
